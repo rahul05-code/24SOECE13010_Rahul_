5e3adc9e96b85e60d6a4ad830ce62e70b70a977d
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-30 10:35:50 +0530

    this week

---
+++ 24SOECE13010_Rahul_/class_obj.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_Rahul_
+{
+
+    public class Employee
+    {
+        int empid;
+        string name;
+        double salary;
+        public void getData(int empid,string name,double salary)
+        {
+            this.empid = empid;
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public void showData()
+        {
+            Console.WriteLine("Employee Id :" + empid);
+            Console.WriteLine("Employee Name :"+name);
+            Console.WriteLine("Salary :"+salary);
+        }
+
+        public double getSalary()
+        {
+            return this.salary;
+        }
+    }
+    internal class class_obj
+    {
+        public static void Main(string[] args)
+        {
+            Employee e1 = new Employee();
+            e1.getData(111,"Rahul",50000);
+            e1.showData();
+
+            Employee e2 = new Employee();
+            e2.getData(112,"Hardik",100000);
+            e2.showData();
+
+            if(e1.getSalary() > e2.getSalary())
+            {
+                e1.showData();
+            }
+            else
+            {
+                e2.showData();
+            }
+        }
+
+    }
+}
---
+++ 24SOECE13010_Rahul_/time.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_Rahul_
+{
+
+    class Time
+    {
+
+        int hour;
+        int minute;
+        int second;
+        public void setTime(int hour,int minute,int second)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+
+        public void getTime()
+        {
+            Console.WriteLine("${0}:${1}:${2}"+hour,minute,second);
+        }
+
+    }
+
+    class timeDisplay
+    {
+        public static void Main(string[] args)
+        {
+            Time t1 = new Time();
+            t1.setTime(11,35,00);
+            t1.getTime();
+        }
+    }
+}
