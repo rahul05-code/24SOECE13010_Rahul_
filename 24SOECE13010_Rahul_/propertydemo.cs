249f87d65495a72fd3eeab61eb9eefa49919cb5b
rahul05-code <kanzariyarahul31@gmail.com>
2025-08-26 10:41:53 +0530

    property demo example diffrent way

--- 24SOECE13010_Rahul_/propertydemo.cs
+++ 24SOECE13010_Rahul_/propertydemo.cs
@@ -11,31 +11,37 @@ namespace _24SOECE13010_Rahul_
         int x; //private member of class
         int y;
 
-        public Demo1(int x, int y)
-        {
-            this.x = x;
-            this.y = y;
-        }
+        //public Demo1(int x, int y)
+        //{
+        //    this.x = x;
+        //    this.y = y;
+        //}
         public int X
         {
-            //set { x = value; }
-            get { return x; }
+            set { x = value; }
+            //get { return x; }
         }
         public int Y
         {
-            //set { y = value; } // initilize member
-            get { return y; } //read-write properties
+            set { y = value; } // initilize member
+            //get { return y; } //read-write properties
+        }
+        void show()
+        {
+            this.y = y;
+            this.x = x;
+
         }
     }
     internal class propertydemo
     {
         public static void Main(string[] args)
         {
-            Demo1 demo = new Demo1(100,200);
+            Demo1 demo = new Demo1();
 
-            //demo.X = 100;
-            //demo.Y = 200;
-
+            demo.X = 10;
+            demo.Y = 20;
+            demo.show();
             Console.WriteLine(demo.X);
             Console.WriteLine(demo.Y);
         }
